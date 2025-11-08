32d4f1f58c9f115053eee03123c7ff392345cd3d
Sofiia-Pukha <sofiia.pukha@gmail.com>
2025-11-08 21:03:41 +0200

    started generator

--- generator.cpp
+++ generator.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <string>
 
+using namespace std;
+
+void generate_file(string filename) 
+{
+    ofstream f(filename);
+    if (f.is_open()) {
+        f << "read 0\n";
+        f << "write 1 55\n";
+        f << "string\n";
+        f.close();
+        cout << "Created " << filename << endl;
+    }
+}
+
+int main() {
+    generate_file("test.txt");
+    return 0;
+}
