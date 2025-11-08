5c38c2be283c2bbe1764f1346ec8ac71c7be8d15
Sofiia-Pukha <sofiia.pukha@gmail.com>
2025-11-08 21:45:33 +0200

    Added random operations generation

--- generator.cpp
+++ generator.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <random>
+#include <ctime>
 
 using namespace std;
 
-void generate_file(string filename) 
+void generate_file(string filename, int lines) 
 {
     ofstream f(filename);
-    if (f.is_open()) {
-        f << "read 0\n";
-        f << "write 1 55\n";
-        f << "string\n";
-        f.close();
-        cout << "Created " << filename << endl;
-    }
+    srand(time(NULL));
+   for(int i=0; i<lines; ++i) 
+   {
+        int r = rand() % 3;
+        if(r == 0) f << "read 0\n";
+        else if(r == 1) f << "write 0 " << rand()%100 << "\n";
+        else f << "string\n";
+    f.close();
 }
 
 int main() {
