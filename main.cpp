8185eec200066c3c07b9c033a5645c0e84774d98
Sofiia-Pukha <sofiia.pukha@gmail.com>
2025-11-10 21:46:09 +0200

    Added data structure class

--- main.cpp
+++ main.cpp
@@ -5,7 +5,31 @@
 
 using namespace std;
 
-void worker(string filename) {
+class MyDataStructure 
+{
+    int field0 = 0;
+    int field1 = 0;
+public:
+    int get_field(int idx) 
+{
+        if (idx == 0) return field0;
+        return field1;
+    }
+    void set_field(int idx, int val) 
+{
+        if (idx == 0) field0 = val;
+        else field1 = val;
+    }
+    operator string() 
+{
+        return "Field0: " + to_string(field0) + ", Field1: " + to_string(field1);
+    }
+};
+
+MyDataStructure shared_data;
+
+void worker(string filename) 
+{
     ifstream f(filename);
     if (!f.is_open()) {
         cout << "Error opening " << filename << endl;
@@ -15,18 +39,22 @@ void worker(string filename) {
     string line, cmd;
     int val;
 
-    while (getline(f, line)) {
+    while (getline(f, line)) 
+    {
         stringstream ss(line);
         ss >> cmd;
-        if (cmd == "read") {
+        if (cmd == "read") 
+        {
             int field;
             ss >> field;
             // cout << "read " << field << endl;
-        } else if (cmd == "write") {
+        } else if (cmd == "write") 
+        {
             int field;
             ss >> field >> val;
             // cout << "write " << field << " " << val << endl;
-        } else if (cmd == "string") {
+        } else if (cmd == "string") 
+        {
             // cout << "string op" << endl;
         }
     }
